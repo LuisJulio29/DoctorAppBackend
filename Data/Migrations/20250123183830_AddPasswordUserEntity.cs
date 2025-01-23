1d0b97b7557045f12103663e47d5f45c07cc630a
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-01-23 14:12:03 -0500

    new migration

---
+++ Data/Migrations/20250123183830_AddPasswordUserEntity.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordUserEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Usarios",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Usarios",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Usarios");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Usarios");
+        }
+    }
+}
