4d71bba2f9f776a310220ffe162e25f788d93e00
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-01-23 14:10:44 -0500

    add password rows to model

--- Models/Entities/Usuario.cs
+++ Models/Entities/Usuario.cs
@@ -10,6 +10,8 @@ namespace Models.Entities
     {
         public int Id { get; set; }
         public String UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
 
     }
 }
