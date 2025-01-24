a388a3fe1a93e98191bd85b90940ccbfae31745a
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-01-24 12:36:59 -0500

    builder service

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 using Data;
 using Microsoft.EntityFrameworkCore;
+using Data.Services;
+using Data.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,7 @@ builder.Services.AddSwaggerGen();
 var conectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(conectionString));
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
