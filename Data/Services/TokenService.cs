16356a83411cc9cbf1bc69db496f44847e6fe35d
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-01-24 12:36:47 -0500

    add TokenService

---
+++ Data/Services/TokenService.cs
@@ -0,0 +1,18 @@
+﻿using Data.Interfaces;
+using Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string CreateToken(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
