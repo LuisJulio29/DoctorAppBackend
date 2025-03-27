181d0a381d7275fdab0e721f98925bb9b44299f8
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-03-27 00:29:57 -0500

    new register, and Usarios --> Usuarios

--- API/Controllers/UsuarioController.cs
+++ API/Controllers/UsuarioController.cs
@@ -19,7 +19,7 @@ namespace API.Controllers
         private readonly ApplicationDbContext _context;
         private readonly ITokenService _tokenService;
 
-        public UsuarioController(ApplicationDbContext context,ITokenService tokenService)
+        public UsuarioController(ApplicationDbContext context, ITokenService tokenService)
         {
             _context = context;
             _tokenService = tokenService;
@@ -29,14 +29,14 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Usuario>>> GetUsarios()
         {
-            return await _context.Usarios.ToListAsync();
+            return await _context.Usuarios.ToListAsync();
         }
 
         // GET: api/Usuario/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Usuario>> GetUsuario(int id)
         {
-            var usuario = await _context.Usarios.FindAsync(id);
+            var usuario = await _context.Usuarios.FindAsync(id);
 
             if (usuario == null)
             {
@@ -44,6 +44,62 @@ namespace API.Controllers
             }
 
             return usuario;
+        }
+        // POST: api/Usuario
+        [HttpPost]
+        public async Task<ActionResult<Usuario>> PostUsuario(RegisterDTO usuario)
+        {
+            if (await UsuarioExists(usuario.UserName))
+            {
+                return BadRequest("El usuario ya existe");
+            }
+            var user = new Usuario
+            {
+                UserName = usuario.UserName
+            };
+            _context.Usuarios.Add(user);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetUsuario", new { id = user.Id }, user);
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserDTO>> Register(RegisterDTO usuario)
+        {
+            if (await UsuarioExists(usuario.UserName))
+            {
+                return BadRequest("El usuario ya existe");
+            }
+            using var hmac = new System.Security.Cryptography.HMACSHA512();
+            var user = new Usuario
+            {
+                UserName = usuario.UserName,
+                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(usuario.Password)),
+                PasswordSalt = hmac.Key
+            };
+            _context.Usuarios.Add(user);
+            await _context.SaveChangesAsync();
+            return new ActionResult<UserDTO>(new UserDTO
+            {
+                UserName = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            });
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDTO>> login(LoginDTO usuario)
+        {
+            var user = await _context.Usuarios.SingleOrDefaultAsync(x => x.UserName == usuario.UserName);
+            if (user == null) return Unauthorized("Usuario no existe");
+            using var hmac = new System.Security.Cryptography.HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(usuario.Password));
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Contraseña incorrecta");
+            }
+            return new ActionResult<UserDTO>(new UserDTO {
+                UserName = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            });
         }
 
         // PUT: api/Usuario/5
@@ -76,66 +132,17 @@ namespace API.Controllers
 
             return NoContent();
         }
-
-        // POST: api/Usuario/Register
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost("Register")]
-        public async Task<ActionResult<UserDTO>> PostUsuario(RegisterDTO registerDTO)
-        {
-            if(await UsuarioExists(registerDTO.UserName))
-            {
-                return BadRequest("UserName already exists");
-            }
-            using var hmac = new System.Security.Cryptography.HMACSHA512();
-            var usuario = new Usuario
-            {
-                UserName = registerDTO.UserName.ToLower(),
-                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(registerDTO.Password)),
-                PasswordSalt = hmac.Key
-            };
-            _context.Usarios.Add(usuario);
-            await _context.SaveChangesAsync();
-            return new UserDTO
-            {
-                UserName = usuario.UserName,
-                Token = _tokenService.CreateToken(usuario)
-            };
-        }
-
-        [HttpPost("Login")]
-        public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
-        {
-            var usuario = await _context.Usarios.SingleOrDefaultAsync(x => x.UserName == loginDTO.UserName);
-            if(usuario == null)
-            {
-                return Unauthorized("Invalid UserName");
-            }
-            using var hmac = new System.Security.Cryptography.HMACSHA512(usuario.PasswordSalt);
-            var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(loginDTO.Password));
-            for (int i = 0; i < computedHash.Length; i++)
-            {
-                if (computedHash[i] != usuario.PasswordHash[i])
-                {
-                    return Unauthorized("Invalid Password");
-                }
-            }
-            return new ActionResult<UserDTO>(new UserDTO
-            {
-                UserName = usuario.UserName,
-                Token = _tokenService.CreateToken(usuario)
-            });
-        }
         // DELETE: api/Usuario/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteUsuario(int id)
         {
-            var usuario = await _context.Usarios.FindAsync(id);
+            var usuario = await _context.Usuarios.FindAsync(id);
             if (usuario == null)
             {
                 return NotFound();
             }
 
-            _context.Usarios.Remove(usuario);
+            _context.Usuarios.Remove(usuario);
             await _context.SaveChangesAsync();
 
             return NoContent();
@@ -143,11 +150,11 @@ namespace API.Controllers
 
         private bool UsuarioExists(int id)
         {
-            return _context.Usarios.Any(e => e.Id == id);
+            return _context.Usuarios.Any(e => e.Id == id);
         }
         private async Task<bool> UsuarioExists(string username)
         {
-            return await _context.Usarios.AnyAsync(e => e.UserName == username.ToLower());
+            return await _context.Usuarios.AnyAsync(e => e.UserName == username);
         }
     }
 }
