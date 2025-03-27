f96df3fa89408d5c408234e1c7023c2b65fb46ab
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-03-27 00:28:01 -0500

    little fix

--- Data/Services/TokenService.cs
+++ Data/Services/TokenService.cs
@@ -15,6 +15,7 @@ namespace Data.Services
     public class TokenService : ITokenService
     {
         private readonly SymmetricSecurityKey _key;
+
         public TokenService(IConfiguration config)
         {
             _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
@@ -26,15 +27,15 @@ namespace Data.Services
                 new Claim(JwtRegisteredClaimNames.NameId, usuario.UserName)
             };
             var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
-            var TokenDescriptor = new SecurityTokenDescriptor
+            var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
                 Expires = DateTime.Now.AddDays(7),
                 SigningCredentials = creds
             };
             var tokenHandler = new JwtSecurityTokenHandler();
-            var token = tokenHandler.CreateToken(TokenDescriptor);
+            var token = tokenHandler.CreateToken(tokenDescriptor);
             return tokenHandler.WriteToken(token);
-        }   
+        }
     }
 }
